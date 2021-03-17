6c22f1f25b4281c1009d9f8cd2b7f26400c32cbe
Timothy Attah <timothyattah733@gmail.com>
2021-03-17 22:40:13 +0100

    Eslint feature

--- server.js
+++ server.js
@@ -2,14 +2,19 @@ const express = require( 'express' );
 const cors = require( 'cors' );
 
 const app = express();
+app.use( cors() );
 
-app.use( cors() )
+const myApp = ( num1, num2 ) => {
+  const result = num1 + num2;
+  return result;
+};
+
+myApp( 5, 5 );
 
 app.get( '/', ( req, res ) => {
-  res.send({hello: 'World', hey: 'there', Hi: 'Wonderful coders'})
-} )
+  res.send( { hello: 'World', hey: 'there', Hi: 'Wonderful coders' } );
+} );
 
 const PORT = process.env.PORT || 5000;
 
 app.listen( PORT, () => console.log( `Server started on port: ${ PORT }` ) );
-
